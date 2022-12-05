480ec39c050716749c1813694a82101db17caee7
IslamelGebaly <islamAlgebaly11@gmail.com>
2022-12-05 15:32:04 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/bc3dcf8b-054d-4743-bce7-74a563ceda49/guestbook/read-guestbook-entries-sequence';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
